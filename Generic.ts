0c1997f0f21ad33fa7743b59aa1a6e710ff3309a
Rajat Verma <16414387+TechnologicalJerry@users.noreply.github.com>
2022-08-05 22:32:36 +0530

    update

---
+++ Generic.ts
@@ -0,0 +1,13 @@
+function getArray<T>(items: T[]): T[] {
+    return new Array<T>().concat(items);
+}
+
+let myNumArr = getArray<number>([100, 200, 300]);
+let myStrArr = getArray<string>(["Hello", "World"]);
+
+myNumArr.push(400);
+myStrArr.push("Hello TypeScript");
+
+myNumArr.push("Hi");
+
+myStrArr.push(500); 
