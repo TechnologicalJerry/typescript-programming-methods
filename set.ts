ec8246a4de230ad870a381efa4e0234b49bbd440
technologicaljerry <cserajatverma@gmail.com>
2023-03-18 23:46:38 +0530

    Create set.ts

---
+++ set.ts
@@ -0,0 +1,13 @@
+et diceEntries = new Set();
+
+diceEntries.add(1).add(2).add(3).add(4).add(5).add(6);
+
+console.log("Dice Entries are:");
+for (let diceNumber of diceEntries) {
+    console.log(diceNumber);
+}
+
+console.log("Dice Entries with forEach are:");
+diceEntries.forEach(function (value) {
+    console.log(value);
+});  
