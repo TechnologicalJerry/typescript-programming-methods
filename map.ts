055dc3b9849c3182d2cbdf5235f8ac3b476e48ca
technologicaljerry <cserajatverma@gmail.com>
2023-03-18 23:47:38 +0530

    Update map.ts

--- map.ts
+++ map.ts
@@ -1,18 +1,18 @@
-let ageMapping = new Map();  
-   
-ageMapping.set("Rakesh", 40);  
-ageMapping.set("Abhishek", 25);  
-ageMapping.set("Amit", 30);  
-   
-for (let key of ageMapping.keys()) {  
-    console.log("Map Keys= " +key);          
-}  
+let ageMapping = new Map();
 
-for (let value of ageMapping.values()) {  
-    console.log("Map Values= " +value);      
-}  
-console.log("The Map Enteries are: ");   
+ageMapping.set("Rakesh", 40);
+ageMapping.set("Abhishek", 25);
+ageMapping.set("Amit", 30);
 
-for (let entry of ageMapping.entries()) {  
-    console.log(entry[0], entry[1]);   
+for (let key of ageMapping.keys()) {
+    console.log("Map Keys= " + key);
+}
+
+for (let value of ageMapping.values()) {
+    console.log("Map Values= " + value);
+}
+console.log("The Map Enteries are: ");
+
+for (let entry of ageMapping.entries()) {
+    console.log(entry[0], entry[1]);
 }  
