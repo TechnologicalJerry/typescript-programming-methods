892b227892b893f46c97901db236719e54f19d54
technologicaljerry <cserajatverma@gmail.com>
2023-03-18 23:49:34 +0530

    Update declaration.ts

--- declaration.ts
+++ declaration.ts
@@ -1,10 +1,10 @@
-var TestSum;    
-(function (TestSum) {    
-   var Calc = (function () {   
-      function Calc() {   
-      }   
-      Calc.prototype.doSum = function (a, b) {  
-         return a + b;  
-      }  
-   })  
+var TestSum;
+(function (TestSum) {
+    var Calc = (function () {
+        function Calc() {
+        }
+        Calc.prototype.doSum = function (a, b) {
+            return a + b;
+        }
+    })
 })  
