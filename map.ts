9c0349f9262acdaa37057ce49dc7e21fee76cff2
Rajat Verma <cserajatverma@gmail.com>
2023-11-26 18:42:51 +0530

    Update map.ts

--- map.ts
+++ map.ts
@@ -5,14 +5,14 @@ ageMapping.set("Abhishek", 25);
 ageMapping.set("Amit", 30);
 
 for (let key of ageMapping.keys()) {
-    console.log("Map Keys= " + key);
+  console.log("Map Keys= " + key);
 }
 
 for (let value of ageMapping.values()) {
-    console.log("Map Values= " + value);
+  console.log("Map Values= " + value);
 }
 console.log("The Map Enteries are: ");
 
 for (let entry of ageMapping.entries()) {
-    console.log(entry[0], entry[1]);
-}  
+  console.log(entry[0], entry[1]);
+}
