8bece6258387795e2d5d759db5f8208f2854e98e
Rajat Verma <cserajatverma@gmail.com>
2023-11-26 18:42:01 +0530

    Update inheritance.ts

--- inheritance.ts
+++ inheritance.ts
@@ -14,9 +14,10 @@ class Audi extends Car {
   display(): void {
 
     console.log("Color of Audi car: " + this.Color);
-    
+
     console.log("Price of Audi car: " + this.Price);
   }
 }
+
 let obj = new Audi(" Black", 8500000);
 obj.display();
