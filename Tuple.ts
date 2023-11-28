3b6894ce60857fcaa56133d94dbdb481a085566e
Rajat Verma <cserajatverma@gmail.com>
2023-11-28 18:29:48 +0530

    Update Tuple.ts

--- Tuple.ts
+++ Tuple.ts
@@ -2,4 +2,4 @@ var employee: [number, string] = [1, "Steve"];
 var person: [number, string, boolean] = [1, "Steve", true];
 
 var user: [number, string, boolean, number, string];
-user = [1, "Steve", true, 20, "Admin"]; 
+user = [1, "Steve", true, 20, "Admin"];
