9b61fd007fa068f9e407ae891db2f70866e69fc7
TechnologicalJerry <cserajatverma@gmail.com>
2025-11-27 15:37:53 +0530

    Add example for Array.prototype.keys usage
    Introduces a new file demonstrating how to use the Array.prototype.keys method to iterate over array indexes for both string and number arrays.

---
+++ array-methods-examples/keys.ts
@@ -0,0 +1,19 @@
+/**
+ * keys – iterator of indexes.
+ */
+
+const sampleNumbers = [1, 2, 3, 4, 5];
+
+const sampleWords = ["alpha", "beta", "gamma", "delta"];
+
+const logExample = (method, scenario, result) => {
+  console.log(`${method} -> ${scenario}:`, result);
+};
+
+(() => {
+  const keysList = Array.from(sampleWords.keys());
+    logExample("keys", "word indexes", keysList);
+  
+    const numberKeys = Array.from(sampleNumbers.keys());
+    logExample("keys", "number indexes", numberKeys);
+})();
